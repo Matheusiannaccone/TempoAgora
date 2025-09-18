775a217e171e255c3d2cc9e1588554fdcd09c4ed
Vitor-Iannaccone <vitor.rosa33@etec.sp.gov.br>
2025-09-18 12:53:23 -0300

    Aula 1

--- TempoAgora/MainPage.xaml.cs
+++ TempoAgora/MainPage.xaml.cs
@@ -1,24 +1,56 @@
-﻿namespace TempoAgora
+﻿using TempoAgora.Models;
+using TempoAgora.Services;
+
+namespace TempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if(t != null)
+                    {
+                        string dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do sol: {t.sunrise} \n" +
+                                         $"Por do sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        /*dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do sol: {t.sunrise} \n" +
+                                         $"Por do sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n";*/
+
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
